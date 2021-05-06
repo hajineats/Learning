554f30426162337836588e69b32ab43b6d859dfb
Hajin Kim <hkim532@aucklanduni.ac.nz>
2021-05-06 19:05:16 +1200

    Props

--- someapp/src/App.js
+++ someapp/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
-const App = () => {
-    const now = new Date()
-    const a = 10
-    const b = 20
+const Hello = (props)=>{
     return (
         <div>
-            <p>Hello World, It is now {now.toString()}</p>
-            <p>
-                {a} plus {b} is {a+b}
-            </p>
+            <p>Hello {props.name}</p>
+        </div>
+    )
+}
+const App = () => {
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name="George"/>
+            <Hello name="Daisy" />
         </div>
     )
 }
