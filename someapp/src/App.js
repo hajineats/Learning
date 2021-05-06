4bbc79541af4b5aa3b73c4822fb6c6aa019498a2
Hajin Kim <hkim532@aucklanduni.ac.nz>
2021-05-06 21:20:20 +1200

    list of notes

--- someapp/src/App.js
+++ someapp/src/App.js
@@ -1,41 +1,12 @@
-import React, {useState} from 'react'
-
-const Button = ({handler, name}) =>{
-    return(
-        <button onClick={handler}>
-            {name}
-        </button>
-    )
-}
-
-const App = () => {
-    const [allClicks, setAll] = useState([])
-
-    const object = {
-        left: 0,
-        right: 0,
-    }
-    const [clicks, setClicks] = useState(object)
-
-
-    const handleLeftClick = ()=>{
-        setClicks({...clicks,left:clicks.left+1})
-        setAll(allClicks.concat('L'))
-    }
-    const handleRightClick = ()=>{
-        setClicks({...clicks,right:clicks.right+1})
-        setAll(allClicks.concat('R'))
-    }
-
+import React from 'react'
+const App = ({notes}) => {
     return (
-        <>
-            {clicks.left}
-            <Button handler={handleLeftClick} name="left" />
-            <Button handler={handleRightClick} name="right" />
-            {clicks.right}
-            <br />
-            {allClicks.join(' ')}
-        </>
+        <div>
+            <ul>
+                {notes.map(note=><li>{note.content}</li>)}
+            </ul>
+        </div>
     )
+
 }
 export default App
